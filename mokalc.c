5602faf649ce4160f1c643ee2cd039a7d6fde5d8
MOKA <76675443+DawnStar907@users.noreply.github.com>
2024-11-13 14:38:42 -0900

    Update mokalc.c

--- mokalc.c
+++ mokalc.c
@@ -3,25 +3,25 @@
 char opr;
 double one, two;
 int main() {
-printf("╭────────────╮\n│   Mokalc   │\n│ written by │\n│   мока <3  │\n╰────────────╯\n\n");
-printf("input operator\n");
-scanf("%c", &opr);
-printf("Enter two operands: ");
-scanf("%lf %lf", &one, &two);
-switch (opr) {
-    case '+':
-        printf("%.1lf + %.1lf = %.1lf", one, two, one + two);
-        break;
-    case '-':
-        printf("%.1lf - %.1lf = %.1lf", one, two, one - two);
-        break;
-    case '*':
-        printf("%.1lf * %.1lf = %.1lf", one, two, one * two);
-        break;
-    case '/':
-        printf("%.1lf / %.1lf = %.1lf", one, two, one / two);
-        break;
-    default:
-        printf("Error! Try again or another Operator, soz.\n");
+    printf("╭────────────╮\n│   Mokalc   │\n│ written by │\n│   мока <3  │\n╰────────────╯\n\n");
+    printf("syntax --> Opr Num Num\n\ninput operation\n");
+    scanf("%c", &opr);
+    scanf("%lf %lf", &one, &two);
+    switch (opr) {
+        case '+':
+            printf("%.1lf + %.1lf = %.1lf\n", one, two, one + two);
+            break;
+        case '-':
+            printf("%.1lf - %.1lf = %.1lf\n", one, two, one - two);
+            break;
+        case '*':
+            printf("%.1lf * %.1lf = %.1lf\n", one, two, one * two);
+            break;
+        case '/':
+            printf("%.1lf / %.1lf = %.1lf\n", one, two, one / two);
+            break;
+        default:
+            printf("Error! Try again or use another Operator, soz.\n");
+    }
 }
-}
+
