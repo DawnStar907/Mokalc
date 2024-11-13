f3a38d4df8af39cfc07c59ccaff98d5f03617d74
MOKA <76675443+DawnStar907@users.noreply.github.com>
2024-11-13 14:42:32 -0900

    Update mokalc.c

--- mokalc.c
+++ mokalc.c
@@ -4,8 +4,8 @@ char opr;
 double one, two;
 int main() {
     printf("╭────────────╮\n│   Mokalc   │\n│ written by │\n│   мока <3  │\n╰────────────╯\n\n");
-    printf("syntax --> Opr Num Num\n\ninput operation\n");
-    scanf("%c %lf %lf", &opr, &one, &two);
+    printf("syntax --> Int Opr Int\n\ninput operation\n");
+    scanf("%lf %c %lf", &one, &opr, &two);
     switch (opr) {
         case '+':
             printf("%.1lf + %.1lf = %.1lf\n", one, two, one + two);
